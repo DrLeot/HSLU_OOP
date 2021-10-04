545d413d8bca2a9b321a2e6726b97c71870966eb
Janik Burkart <janik.burkart@polygon-solutions.ch>
2021-10-04 08:47:30 +0200

    init
    init

---
+++ VL2_Datentypen/src/com/company/Datentypen.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Datentypen {
+    public static void main(String[] args) {
+
+        System.out.println(Integer.MAX_VALUE + 1); // prints -2147483648
+    }
+}
---
+++ VL2_Datentypen/src/com/company/Temperatur.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Temperatur {
+    final float CELSIUS_OFFSET = (float) 273.15;
+    private float tempcelsius;
+
+    /***
+     * Constructor. Sets the temperature in celsius
+     */
+    public Temperatur(){
+        this.tempcelsius = 20;
+    }
+    public Temperatur(final float tempvalue){
+        this.tempcelsius = tempvalue;
+    }
+
+    /** GETTERS **/
+    public float getTempcelsius(){
+        return this.tempcelsius;
+    }
+    public float getTempkelvin(){
+        return this.tempcelsius + CELSIUS_OFFSET;
+    }
+    public float getTempFahrenheit(){
+        return this.tempcelsius * (float) 1.8 + 32;
+    }
+
+
+    public void setTempcelsius(final float tempcelsius){
+        this.tempcelsius = tempcelsius;
+    }
+    public void adjustTempcelsius(final float difference){
+        this.tempcelsius += difference;
+    }
+
+}
